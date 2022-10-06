996ea15a127d905f949b0c297a3993e2b924f774
Ridha Watanabe <xsjark@protonmail.com>
2022-10-06 10:20:47 -0500

    part 2 done

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
-const canvas = document.getElementById("myCanvas")
-const ctx = canvas.getContext("2d")
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-const drawRectangle = () => {
+function drawBall() {
     ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = "#FF0000";
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
-const drawCircle = () => {
-    ctx.beginPath();
-    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
 }
 
-const outlineRectangle = () => {
-    ctx.beginPath();
-    ctx.rect(160, 10, 100, 40);
-    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.stroke();
-    ctx.closePath();
-
-}
-
-drawRectangle()
-drawCircle()
-outlineRectangle()
+// setInterval(draw, 10);
